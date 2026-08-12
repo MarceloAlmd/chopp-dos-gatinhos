567f4601d16d8f1fd782d2056559fd80446b876d
Marcelo <marcelo3x4@hotmail.com>
2026-08-11 21:11:06 -0300

    created Title and description, created too search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Products } from "@/templates/product";
+
 export default function Home() {
-  return;
+  return <Products />;
 }
---
+++ src/templates/product/product.tsx
@@ -0,0 +1,26 @@
+import { Search } from "lucide-react";
+
+export function Products() {
+  return (
+    <div className="max-w-6x1 mx-auto px-4 sm:px-6 md:px-16 lg:px-40 xl:px-60">
+      <div className="flex flex-col gap-4 max-w-2xs mt-3 md:mt-7">
+        <h1 className="flex items-center gap-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-brand-gray-300">
+          Nosso Chopp&apos;s
+        </h1>
+
+        <p className="font-medium text-brand-gray-300">
+          Feitos com carinho para refrescar você e ajudar quem mais precisa.
+        </p>
+
+        <div className="flex items-center gap-4 pl-2.5 border border-brand-gray-100 rounded-sm">
+          <Search size={20} />
+          <input
+            className="py-2.5 w-full outline-0"
+            placeholder="Buscar sabor..."
+            type="text"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
