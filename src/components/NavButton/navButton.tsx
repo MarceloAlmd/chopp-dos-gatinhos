a57cb781c9715a0ee7a2bec5206e75d6b81c975e
Marcelo <marcelo3x4@hotmail.com>
2026-08-14 21:10:26 -0300

    ativa o button com forme a navegação

--- src/components/NavButton/navButton.tsx
+++ src/components/NavButton/navButton.tsx
@@ -1,15 +1,30 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type NavButtonProps = {
   title: string;
 };
 
 export function NavButton({ title }: NavButtonProps) {
+  const pathname = usePathname();
+
+  const href = title === "Produtos" ? "/" : "/por-que-comprar";
+  const isActive = pathname === href;
+
   return (
     <nav className="flex gap-4 text-brand-gray-100">
       <Link
-        href="/"
-        className="text-[12px] sm:text-[16px] border-b border-transparent pb-0.5 transition-all hover:border-brand-gray-100"
+        href={href}
+        className={`
+            text-[12px] sm:text-[16px] border-b pb-0.5 transition-all
+            ${
+              isActive
+                ? "border-brand-gray-100"
+                : "border-transparent hover:border-brand-gray-100"
+            }
+          
+          `}
       >
         {title}
       </Link>
